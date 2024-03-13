e38edc213ed777b8dea7d7cb4d1288d5714f8e61
debelEXO <161918683+debelEXO@users.noreply.github.com>
2024-03-13 22:49:36 +0530

    Add files via upload

---
+++ js/onScroll.js
@@ -0,0 +1,19 @@
+let sections =  document.querySelectorAll('section');
+
+window.onscroll = ()=> {
+    sections.forEach(sec => {
+        let top  = window.scrollY +170 ;
+        let offset  = sec.offsetTop -150;
+        let height = sec.offsetHeight;
+
+        if(top>= offset && top<offset + height){
+            sec.classList.add('show-animate');
+            
+        }
+        // else{
+        //     sec.classList.remove('show-animate');
+
+        // }
+    
+    })
+}
